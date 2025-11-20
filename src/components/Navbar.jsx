30cedd7afd1b6875929fb46183d495d188ae6613
Dragomir-Alex <dragomario2k@gmail.com>
2025-11-20 12:29:34 +0200

    Added zigzag separator to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,22 @@ const Navbar = () => {
     { label: "Contact", href: "#contact" },
   ];
 
+  const zigzag = {
+    "--m": `
+      conic-gradient(from -45deg at bottom, #0000, #000 1deg 89deg, #0000 90deg) bottom/40px 20px repeat-x,
+      linear-gradient(#000 0 0) top/100% calc(100% - 20px) no-repeat
+    `,
+    WebkitMask: "var(--m)",
+    mask: "var(--m)",
+  };
+
   const handleNavClick = (href) => {};
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-white shadow-[0px_5px_rgba(0,_98,_90,_0.4),_0px_10px_rgba(0,_98,_90,_0.3),_0px_15px_rgba(0,_98,_90,_0.2),_0px_20px_rgba(0,_98,_90,_0.1),_0px_25px_rgba(0,_98,_90,_0.05)]">
+    <nav
+      className="fixed pb-6 pt-1 top-0 left-0 right-0 z-50 bg-amber-300"
+      style={zigzag}
+    >
       <div className="flex items-center gap-20 px-10">
         {navItems.map((item) => (
           <AnimatedButton
